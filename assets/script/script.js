25a0732bc96170034c4a5d9f6d1d28be12ae76d6
Ruan Figuereido <huanfiguereido@gmail.com>
2023-08-27 14:42:07 -0300

    finalizadas as funções que avançam e voltam nos passos dos formulários

--- assets/script/script.js
+++ assets/script/script.js
@@ -47,13 +47,6 @@ function previousStep(item){
 function activeStep(){
     cont>0?btnGoBack.style.display="flex":btnGoBack.style.display="none";
     switch(cont){
-        //preciso arrumar esta secão para que o botão de previa e d proximo exibam corretamente os articles com o conteudo da tela;
-        // case 0: field[2].style.display="none";field[0].style.display="flex";spans[0].style.backgroundColor="rgb(185, 227, 213)";spans[3].style.backgroundColor="transparent";
-        // case 1: field[0].style.display="none";field[1].style.display="flex";spans[1].style.backgroundColor="rgb(185, 227, 213)";break;
-        // case 2: field[1].style.display="none";field[2].style.display="flex";spans[2].style.backgroundColor="rgb(185, 227, 213)";spans[1].style.backgroundColor="transparent";break;
-        // case 3: field[2].style.display="none";field[3].style.display="flex";spans[3].style.backgroundColor="rgb(185, 227, 213)";spans[2].style.backgroundColor="transparent";break;
-        // case 4: field[3].style.display="none";field[4].style.display="flex";spans[3].style.backgroundColor="transparent";spans[2].style.backgroundColor="transparent";break;
-        //pra cada case uma função especifica
         case 0: caseZero();break;
         case 1: caseOne();break;
         case 2: caseTwo();break;
@@ -65,7 +58,7 @@ function caseZero(){
     for(let i=0; i<5; i++){
         spans[i].style.backgroundColor="transparent";
     }
-    spans[0].style.backgroundColor="rgb(185, 227, 213)"//
+    spans[0].style.backgroundColor="rgb(185, 227, 213)";
     for(let i=0; i<5; i++){
         field[i].style.display="none";
     }
@@ -75,7 +68,7 @@ function caseOne(){
     for(let i=0; i<5; i++){
         spans[i].style.backgroundColor="transparent";
     }
-    spans[1].style.backgroundColor="rgb(185, 227, 213)";//
+    spans[1].style.backgroundColor="rgb(185, 227, 213)";
     for(let i=0; i<5; i++){
         field[i].style.display="none";
     }
@@ -85,7 +78,7 @@ function caseTwo(){
     for(let i=0; i<5; i++){
         spans[i].style.backgroundColor="transparent";
     }
-    spans[2].style.backgroundColor="rgb(185, 227, 213)";//
+    spans[2].style.backgroundColor="rgb(185, 227, 213)";
     for(let i=0; i<5; i++){
         field[i].style.display="none";
     }
@@ -95,7 +88,7 @@ function caseTree(){
     for(let i=0; i<5; i++){
         spans[i].style.backgroundColor="transparent";
     }
-    spans[3].style.backgroundColor="rgb(185, 227, 213)";//
+    spans[3].style.backgroundColor="rgb(185, 227, 213)";
     for(let i=0; i<5; i++){
         field[i].style.display="none";
     }
@@ -105,11 +98,16 @@ function caseFour(){
     for(let i=0; i<5; i++){
         spans[i].style.backgroundColor="transparent";
     }
-    spans[4].style.backgroundColor="rgb(185, 227, 213)";//
+    spans[4].style.backgroundColor="rgb(185, 227, 213)";
     for(let i=0; i<5; i++){
         field[i].style.display="none";
     }
-    field[4].style.display="flex";
+    field[4].style.display="flex";//
+    let finalBtnArea = document.querySelector("#btns");
+    finalBtnArea.style.display="none";
+    setTimeout(x=>{
+        window.location.reload();
+    },3000);
 }
 
 
